96177baec4b8bef238a4cc85b8b004415d9b590a
gsus <barrazajesusarmando@gmail.com>
2024-02-07 11:41:21 -0500

    frontend basico

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,75 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Consulta de Disponibilidad</h1>
+    <input v-model="sitio" type="text" />
+    <br>
+    <button @click="consultarDisponibilidad">Consultar Disponibilidad</button>
+    <div v-if="disponibilidad">
+      <h2>Resultado:</h2>
+      <pre>{{ disponibilidad }}</pre>
+    </div>
+
+    <h1>Consulta de Tarifas</h1>
+    <button @click="consultarTarifas">Consultar Tarifas</button>
+    <div v-if="tarifas">
+      <h2>Resultado:</h2>
+      <pre>{{ tarifas }}</pre>
+    </div>
+
+    <h1>Calcular Tarifa a Cancelar</h1>
+    <button @click="calcularTarifaCancelar">Calcular Tarifa</button>
+    <div v-if="tarifaCancelar">
+      <h2>Resultado:</h2>
+      <pre>{{ tarifaCancelar }}</pre>
+    </div>
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      sitio: '',
+      disponibilidad: null,
+      tarifas: null,
+      tarifaCancelar: null,
+    };
+  },
+  methods: {
+    async consultarDisponibilidad() {
+      try {
+        const respuesta = await axios.get(
+          "http://localhost:3000/api/disponibilidad/" + this.sitio
+        );
+        this.disponibilidad = respuesta.data.disponibilidad;
+      } catch (error) {
+        console.error("Error al consultar disponibilidad:", error);
+      }
+    },
+    async consultarTarifas() {
+      try {
+        const respuesta = await axios.get(
+          "http://localhost:3000/api/tarifas"
+        );
+        this.tarifas = respuesta.data.tarifas;
+      } catch (error) {
+        console.error("Error al consultar tarifas:", error);
+      }
+    },
+    async calcularTarifaCancelar() {
+      try {
+        const respuesta = await axios.get(
+           "http://localhost:3000/api/calcular-tarifa"
+        );
+        this.tarifaCancelar = respuesta.data.tarifa;
+      } catch (error) {
+        console.error("Error al calcular tarifa a cancelar:", error);
+      }
+    },
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
